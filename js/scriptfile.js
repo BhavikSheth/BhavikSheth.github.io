f38c101652b1d007a34de6cd60bcbb9522686f48
Bhavik Sheth <Bhavikaaaa@macbook-air.mynet>
2017-03-25 13:12:54 +0000

    make portfolio responsive, add hover overlay over projects

--- js/scriptfile.js
+++ js/scriptfile.js
@@ -4,4 +4,15 @@ $(window).scroll(function() {
 	var scrolled = winTop/(docHeight-winHeight)*100;
 	$('#scroll-line').css('width', (scrolled + '%'));
 });
-
+/*
+$(document).ready(function() {
+  $(".scroll").click(function(event) {
+    event.preventDefault();
+    $("html,body").animate({
+      scrollTop: $(this.hash).offset().top
+    }, 500);
+    $('.navbar-default a').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
+*/
